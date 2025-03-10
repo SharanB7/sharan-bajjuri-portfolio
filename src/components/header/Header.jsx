4f12f8295df1eb0fdaad33dedc8f4bd0fe54c146
SharanB7 <bajjurisharan@gmail.com>
2025-03-09 19:28:55 -0700

    Modified main page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,9 +14,9 @@ const Header = () => {
         <CTA/>
         <HeaderSocials/>
 
-        <div className='me'>
-          {/* <img src={ME} alt="me"></img> */}
-        </div>
+        {/* <div className='me'>
+          <img src={ME} alt="me"></img>
+        </div> */}
 
         <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
